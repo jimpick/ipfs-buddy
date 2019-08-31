3f1ecd061246e9a09cfdb23f135b1f678e5bc4cf
Jim Pick <jim@backblend.com>
2019-08-30 22:55:17 -0700

    Sort by most recent

--- peer-bandwidth.js
+++ peer-bandwidth.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const ipfsClient = require('ipfs-http-client')
+const delay = require('delay')
 
 // curl "http://localhost:5001/api/v0/log/tail"
 
@@ -14,23 +15,49 @@ async function run () {
   const buddyId = buddyIdentity.id
   console.log('Id:', buddyId)
 
-  const peerInfos = await ipfs.swarm.peers({ verbose: true })
-  for (const peerInfo of peerInfos) {
-    // console.log(peerInfo)
-    const { addr, peer, muxer, latency, streams } = peerInfo
-    const protos = new Set()
-    if (streams) {
-      for (const { Protocol: proto } of streams) {
-        protos.add(proto.replace(/\/ipfs\//, '').replace(/\/.*$/, ''))
+  let lineTimes = {}
+
+  while (true) {
+    const peerInfos = await ipfs.swarm.peers({ verbose: true })
+    const lines = []
+    for (const peerInfo of peerInfos) {
+      const { addr, peer, muxer, latency, streams } = peerInfo
+      const protos = new Set()
+      if (streams) {
+        for (const { Protocol: proto } of streams) {
+          const shortProto = proto.replace(/\/ipfs\//, '').replace(/\/.*$/, '')
+          if (shortProto !== '') protos.add(shortProto)
+        }
+      }
+      lines.push(
+        `${peer.toB58String()} ` +
+        `${addr} ` +
+        `${latency !== 'n/a' && latency !== '' ? latency + ' ' : ''}` +
+        `${[...protos].sort().join(' ')}`
+      )
+    }
+    const now = Date.now()
+    const newLineTimes = {}
+    for (const line of lines) {
+      if (lineTimes[line]) {
+        newLineTimes[line] = lineTimes[line]
+      } else {
+        newLineTimes[line] = now
       }
     }
-    // console.log('Jim', protos)
-    console.log(
-      `${peer.toB58String()} ` +
-      `${addr} ` +
-      `${latency !== 'n/a' ? latency + ' ' : ''}` +
-      `${[...protos].sort().join(' ')}`
-    )
+    lineTimes = newLineTimes
+    const sortedLines = Object.entries(lineTimes).sort(
+      ([line1, time1], [line2, time2]) => {
+        const timeSort = time2 - time1
+        if (timeSort !== 0) return timeSort
+        return line1.localeCompare(line2)
+      }
+    ).map(([line, time]) => time + '|' + line)
+
+    console.log('\u001b[2J\u001b[0;0H')
+    sortedLines.length = process.stdout.rows - 3
+    console.log(sortedLines.join('\n'))
+    await delay(1000)
   }
 }
 
